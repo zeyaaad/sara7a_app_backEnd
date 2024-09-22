acc66c5ade9dac4045ff53146a1923af16f7dc10
Zeyad Mohamed <114029186+zeyaaad@users.noreply.github.com>
2024-09-23 01:42:34 +0300

    Update sendEmail.js

--- src/email/sendEmail.js
+++ src/email/sendEmail.js
@@ -21,7 +21,7 @@ const transporter = nodemailer.createTransport({
     to: data.email, 
     from: ' "Sara7a plus App"', 
     subject: "Sara7a plus App", 
-    html: EmailTamplate(data.api,data.email), 
+    html: "<h1> Hamada Email </h1>", 
   });
 
 
